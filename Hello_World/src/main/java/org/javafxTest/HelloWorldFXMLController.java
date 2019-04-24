1609d6b91ebe0d28cd09ff3c99d3f8bc369aac8d
Lumargon <luciamargon96@gmail.com>
2019-04-24 11:15:00 +0200

    Haciendo Commit para añadir cosas desde local

---
+++ Hello_World/src/main/java/org/javafxTest/HelloWorldFXMLController.java
@@ -0,0 +1,25 @@
+package org.albfan.javafxTest;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloWorldFXMLController implements Initializable {
+
+    @FXML
+    private Button button;
+
+    @FXML
+    private Button controllerButton;
+
+    @FXML
+    void onAction(ActionEvent event) {
+        System.out.println("Hello World! (controllerButton)");
+    }
+
+}
